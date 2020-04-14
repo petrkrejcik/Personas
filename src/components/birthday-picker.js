35c9e76c21ab78db030f3a4fddee47f508cbdc7c
Petr Krejčík <pkrejcik@soincon.es>
2020-04-14 23:03:24 +0200

    date

--- src/components/birthday-picker.js
+++ src/components/birthday-picker.js
@@ -1,9 +1,12 @@
-export const birthdayPicker = ({day, month, year}, onChange) => {
+export const birthdayPicker = ({ day, month, year }, onChange) => {
 	const createOptions = (start, end, selected) => {
-		const options = Array.from({length: end + 1 - start}, (v, k) => start + k);
-		return options.map(i => {
-			const option = document.createElement('option');
-			const value = i + '';
+		const options = Array.from(
+			{ length: end + 1 - start },
+			(v, k) => start + k
+		);
+		return options.map((i) => {
+			const option = document.createElement("option");
+			const value = i + "";
 			option.innerText = value;
 			option.value = value;
 			if (selected && value === selected) option.selected = true;
@@ -11,22 +14,24 @@ export const birthdayPicker = ({day, month, year}, onChange) => {
 		});
 	};
 
-	const dayEl = document.createElement('select');
-	const monthEl = document.createElement('select');
-	const yearEl = document.createElement('select');
-	createOptions(1, 31, day).forEach(option => dayEl.appendChild(option));
-	createOptions(1, 12, month).forEach(option => monthEl.appendChild(option));
-	createOptions(1900, 2019, year).forEach(option => yearEl.appendChild(option));
+	const dayEl = document.createElement("select");
+	const monthEl = document.createElement("select");
+	const yearEl = document.createElement("select");
+	createOptions(1, 31, day).forEach((option) => dayEl.appendChild(option));
+	createOptions(1, 12, month).forEach((option) => monthEl.appendChild(option));
+	createOptions(1900, new Date().getUTCFullYear(), year).forEach((option) =>
+		yearEl.appendChild(option)
+	);
 
 	/**
 	 * @param {string} type
 	 * @returns {(HTMLInputElement) => void} type
 	 */
-	const onChangeCb = (type) => (ev) => onChange(type, (ev.target).value);
+	const onChangeCb = (type) => (ev) => onChange(type, ev.target.value);
 
-	dayEl.addEventListener('change', onChangeCb('day'));
-	monthEl.addEventListener('change', onChangeCb('month'));
-	yearEl.addEventListener('change', onChangeCb('year'));
+	dayEl.addEventListener("change", onChangeCb("day"));
+	monthEl.addEventListener("change", onChangeCb("month"));
+	yearEl.addEventListener("change", onChangeCb("year"));
 
 	return [dayEl, monthEl, yearEl];
 };
